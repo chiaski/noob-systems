18f7dd319ab8ca667b31e736500426affc72857d
Chia <chiamisola@gmail.com>
2020-02-26 21:45:06 -0500

    More places

--- public/app/main.js
+++ public/app/main.js
@@ -94,28 +94,70 @@ var watchers = [
         coordinates: "43.649860, 13.048160"
     },
     {
-        title: "Colorado, Colorado Springs",
-        link: "http://162.245.149.145/mjpg/video.mjpg",
-        geography: "Colorado, Colorado Springs, USA",
-        coordinates: "38.791779, -104.826480"
+        title: "Hofuoborgarsvaoio, Reykjavik",
+        link: "http://157.157.19.131/mjpg/video.mjpg",
+        geography: "Hofuoborgarsvaoio, Reykjavik, Iceland",
+        coordinates: "64.135480, -21.895410"
     },
     {
-        title: "Colorado, Colorado Springs",
-        link: "http://162.245.149.145/mjpg/video.mjpg",
-        geography: "Colorado, Colorado Springs, USA",
-        coordinates: "38.791779, -104.826480"
+        title: "PRINTER",
+        link: "http://152.3.225.195:8080/?action=stream",
+        geography: "North Carolina, Durham, USA",
+        coordinates: "35.996611, -78.899080"
     },
     {
-        title: "Colorado, Colorado Springs",
-        link: "http://162.245.149.145/mjpg/video.mjpg",
-        geography: "Colorado, Colorado Springs, USA",
-        coordinates: "38.791779, -104.826480"
+        title: "Lazio, Latina",
+        link: "http://79.61.177.207:83/mjpg/video.mjpg",
+        geography: "Lazio, Latina, Italy",
+        coordinates: "41.466140, 12.904300"
     },
     {
-        title: "Colorado, Colorado Springs",
-        link: "http://162.245.149.145/mjpg/video.mjpg",
-        geography: "Colorado, Colorado Springs, USA",
-        coordinates: "38.791779, -104.826480"
+        title: "GOD",
+        link: "http://176.35.183.10:1024/mjpg/video.mjpg",
+        geography: "England, Leeds, UK",
+        coordinates: "51.246770, 0.606820"
+    },
+    {
+        title: "FOR SALE",
+        link: "http://63.40.185.220:82/cgi-bin/camera?resolution=640&amp;quality=1&amp;Language=0&amp;1582771146",
+        geography: "Texas, Houston, USA",
+        coordinates: "29.763280, -95.363270"
+    },
+    {
+        title: "Avtonomna Respublika Krym, Simferopol",
+        link: "http://91.247.97.211:8084/?action=stream",
+        geography: "Avtonomna Respublika Krym City, Simferopol, Ukraine",
+        coordinates: "44.957190, 34.110790"
+    },
+    {
+        title: "Bahia, Cachoeira",
+        link: "http://187.1.147.77/mjpg/video.mjpg",
+        geography: "Bahia, Cachoeira, Brazil",
+        coordinates: "-12.618330, -38.955830"
+    },
+    {
+        title: "???",
+        link: "http://84.232.147.36:8080/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER",
+        geography: "Cluj, Salciua De Sus, Romania",
+        coordinates: "46.400000, 23.416670"
+    },
+    {
+        title: "Seoul-Teukbyeolsi, Seoul",
+        link: "http://211.211.61.110:8000/webcapture.jpg?command=snap&channel=1?1582771369",
+        geography: "Seoul-Teukbyeolsi, Seoul, Korea",
+        coordinates: "37.568260, 126.977830"
+    },
+    {
+        title: "Hamadan, Hamadan",
+        link: "http://2.185.149.180/cgi-bin/snapshot.cgi?chn=0&u=admin&p=&q=0&1582771411",
+        geography: "Hamadan, Hamadan, Iran",
+        coordinates: "34.799220, 48.514560"
+    },
+    {
+        title: "New Jersey, Mount Laurel",
+        link: "http://96.91.239.26:1024/mjpg/video.mjpg",
+        geography: "New Jersey, Mount Laurel, USA",
+        coordinates: "39.947820, -74.911680"
     }
 ]
 
